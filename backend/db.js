dd52220a27948eb71997e186ca062a1d59bcc672
Christian Sanchez <christian.sanchez@correo.unimet.edu.ve>
2025-03-22 13:22:46 -0400

    probando mongo db, ya creo que la base de datos con todas las colecciones, probe registrarme y funciono, falata arreglos

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://christiansanchez:V7juIzIPPPRPGbCk@avilametcluster.ggjnh.mongodb.net/', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('✅ Conectado a MongoDB Atlas'))
-.catch((err) => console.error('❌ Error de conexión:', err));
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://christiansanchez:V7juIzIPPPRPGbCk@avilametcluster.ggjnh.mongodb.net/AvilaMet');
+    console.log('✅ Conectado a MongoDB Atlas');
+  } catch (err) {
+    console.error('❌ Error de conexión:', err);
+  }
+};
+
+module.exports = connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const connectDB = require("./db");
+const cors = require("cors");
+const connectDB = require("./db"); // Importa la función connectDB
 const app = express();
 const port = process.env.PORT || 5000;
 
 require("dotenv").config();
-connectDB();
 
+// Conecta a la base de datos
+connectDB(); // Llama a la función connectDB
+
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/destinations', require('./routes/destinationRoutes'));
@@ -13,6 +17,11 @@ app.use('/api/excursions', require('./routes/excursionRoutes'));
 app.use('/api/posts', require('./routes/postRoutes'));
 app.use('/api/reservations', require('./routes/reservationRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
+
+// Ruta de prueba
+app.get('/test', (req, res) => {
+  res.send('El backend está funcionando correctamente');
+});
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
